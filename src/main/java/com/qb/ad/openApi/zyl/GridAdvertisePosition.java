61c6a0de543d2049ab98ad621cd90895958106be
zhaoyanfei-git <545869238@qq.com>
2021-04-22 13:16:25 +0800

    修改parkid的字段为gridid

--- src/main/java/com/qb/ad/openApi/zyl/GridAdvertisePosition.java
+++ src/main/java/com/qb/ad/openApi/zyl/GridAdvertisePosition.java
@@ -18,8 +18,8 @@ import static com.qb.ad.util.ApiUtilsTest.*;
  */
 public class GridAdvertisePosition {
     static String traffic_name = "ZYL流量主Auto1119113247"; // 需要新增车场广告位的流量主  运营管理-流量主列表
-    static String adPosIds = "1#2#3#4#5";//1#2#3#4#5  1入场推送，2出场推送，3输入车牌 ，4支付页面，5付款成功,6自定义广告页···
-    static String scene = "3";//场景枚举如下
+    static String adPosIds = "6";//1#2#3#4#5  1入场推送，2出场推送，3输入车牌 ，4支付页面，5付款成功,6自定义广告页···
+    static String scene = "11";//场景枚举如下
     //3场内停车4路测停车5汽车充电桩6洗车7按摩椅8体重秤9摇摇车10兑币机11娃娃机12唱歌机13互动拍照14商场大屏互动游戏
     // 15店头互动游戏16互动游戏17售货机18共享纸巾19共享雨伞20共享打印机21共享充电宝22洗衣机23派样机24聚合支付25聚合设备26智慧迎宾屏幕
 
@@ -33,15 +33,17 @@ public class GridAdvertisePosition {
         String partnerId = selectMap.get("partner_id").toString();
         String private_key = selectMap.get("private_key").toString();
         //创建场地——根据traffic_name新增场地
-        String parkId = createGrid(partnerId, private_key, scene);
-        //更新场地信息
-        updateGrid(parkId, partnerId, private_key, scene);
-        //获取车场
-        getGrid(parkId, partnerId);
+      // String gridId = createGrid(partnerId, private_key, scene);
+       //更新场地信息
+        String gridId ="2021gridID0422111410";
+        updateGrid(gridId, partnerId, private_key, scene);
+       /*  //获取车场
+        getGrid(gridId, partnerId);*/
         //新增广告页
-        createSceneAdvertisePosition(adPosIds, parkId, partnerId, private_key);
-        //获取广告也
-        getSceneAdvertisePosition(parkId, partnerId);
+      /*  String gridId ="2021gridID0422111410";
+       createSceneAdvertisePosition(adPosIds, gridId, partnerId, private_key);*/
+       /* //获取广告也
+        getSceneAdvertisePosition(gridId, partnerId);*/
     }
 
 }

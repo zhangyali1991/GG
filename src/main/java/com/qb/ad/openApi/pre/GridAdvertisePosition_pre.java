a0540432e5aa5bd0105b4eef54ea026b59dd968f
zhaoyanfei-git <545869238@qq.com>
2021-05-12 14:52:50 +0800

    增加新增

--- src/main/java/com/qb/ad/openApi/pre/GridAdvertisePosition_pre.java
+++ src/main/java/com/qb/ad/openApi/pre/GridAdvertisePosition_pre.java
@@ -1,5 +1,6 @@
 package com.qb.ad.openApi.pre;
 
+import com.qb.ad.entity.DataPre;
 import com.qb.ad.jdbc.MysqlDeal;
 import org.testng.annotations.Test;
 import java.util.Map;
@@ -11,34 +12,26 @@ import static com.qb.ad.util.ApiUtilsPre.*;
  * @date: 2021/4
  */
 public class GridAdvertisePosition_pre {
-    static String traffic_name = "ZYL流量主Auto1119113247"; // 需要新增车场广告位的流量主  运营管理-流量主列表
-    static String adPosIds = "39";//1#2#3#4#5  1入场推送，2出场推送，3输入车牌 ，4支付页面，5付款成功,6自定义广告页···
+    static String adPosIds = "6";//1#2#3#4#5  1入场推送，2出场推送，3输入车牌 ，4支付页面，5付款成功,6自定义广告页···
     static String scene = "26";//场景枚举如下
     //3场内停车4路测停车5汽车充电桩6洗车7按摩椅8体重秤9摇摇车10兑币机11娃娃机12唱歌机13互动拍照14商场大屏互动游戏
     // 15店头互动游戏16互动游戏17售货机18共享纸巾19共享雨伞20共享打印机21共享充电宝22洗衣机23派样机24聚合支付25聚合设备26智慧迎宾屏幕
 
     @Test
     public static void createGridAdvertisePosition() throws Exception {
-        //根据流量主名partnerId查出partnerId和private_key
-        String select_sql = "SELECT partner_id,private_key FROM traffic_info WHERE traffic_name = '" + traffic_name + "'";
-        String[] str = new String[]{"partner_id", "private_key"};
-        Map <String, String> selectMap = MysqlDeal.mysqlDeal(select_sql, str);
-        System.out.print("selectMap: " + selectMap);
-        String partnerId = selectMap.get("partner_id").toString();
-        String private_key = selectMap.get("private_key").toString();
+
         //创建场地——根据traffic_name新增场地
-        //  String gridId ="2021场地ID0422141129";
-       String gridId = createGrid(partnerId, private_key, scene);
+      // String gridId = createGrid(DataPre.partnerId, DataPre.priv_key, scene);
        //更新场地信息
-       // String gridId ="2021gridID0510174821";
-        updateGrid(gridId, partnerId, private_key, scene);
+        String gridId ="2021gridID0512145102";
+      //  updateGrid(gridId,DataPre.partnerId, DataPre.priv_key, scene);
          //获取车场
         //String gridId ="";
-        getGrid(gridId, partnerId);
+       // getGrid(gridId, DataPre.partnerId);
         //新增广告页
-       createSceneAdvertisePosition(adPosIds, gridId, partnerId, private_key);
+       createSceneAdvertisePosition(adPosIds, gridId, DataPre.partnerId, DataPre.priv_key);
         //获取广告也
-        getSceneAdvertisePosition(gridId, partnerId);
+       // getSceneAdvertisePosition(gridId, DataPre.partnerId);
     }
 
 }
